45bc48a4fa3cedbfd06f0d3d919584a45a96e060
Satyam Vishwakarma <v.satyam@op.iitg.ac.in>
2025-05-29 22:37:17 +0530

    product id error solved

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,22 +1,34 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { getUserFromToken } from "@/lib/auth";
+import { cookies } from "next/headers";
 
-export async function GET(_, { params }) {
+export async function GET(_, context) {
+  const params = await context.params;
   const product = await prisma.product.findUnique({
     where: { id: params.id },
   });
+  if (!product) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
   return NextResponse.json(product);
 }
 
-export async function PUT(req, { params }) {
-  const user = await getUserFromToken();
+export async function PUT(req, context) {
+  const params = await context.params;
+  const token = cookies().get("token")?.value;
+  const user = await getUserFromToken(token);
 
-  if (user?.role !== "admin") {
+  if (user?.role !== "ADMIN") {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   const data = await req.json();
+  const product = await prisma.product.findUnique({ where: { id: params.id } });
+  if (!product) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+
   const updatedProduct = await prisma.product.update({
     where: { id: params.id },
     data,
@@ -25,11 +37,18 @@ export async function PUT(req, { params }) {
   return NextResponse.json(updatedProduct);
 }
 
-export async function DELETE(_, { params }) {
-  const user = await getUserFromToken();
+export async function DELETE(_, context) {
+  const params = await context.params;
+  const token = cookies().get("token")?.value;
+  const user = await getUserFromToken(token);
 
-  if (user?.role !== "admin") {
+  if (user?.role !== "ADMIN") {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const product = await prisma.product.findUnique({ where: { id: params.id } });
+  if (!product) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
   }
 
   await prisma.product.delete({
